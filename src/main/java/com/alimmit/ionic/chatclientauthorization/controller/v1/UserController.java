c66f6a23c03e39489d3e932d4f4ae83c75601388
timmila <tim.mila@7summitsagency.com>
2017-10-10 22:32:20 -0500

    Removed token create on register

--- src/main/java/com/alimmit/ionic/chatclientauthorization/controller/v1/UserController.java
+++ src/main/java/com/alimmit/ionic/chatclientauthorization/controller/v1/UserController.java
@@ -5,10 +5,12 @@ import com.alimmit.ionic.chatclientauthorization.service.OAuthLoginService;
 import com.alimmit.ionic.chatclientauthorization.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,16 +19,14 @@ public class UserController {
     private static final Log LOG = LogFactory.getLog(UserController.class);
 
     private final UserService userService;
-    private final OAuthLoginService oAuthLoginService;
 
-    public UserController(final UserService userService, final OAuthLoginService oAuthLoginService) {
+    public UserController(final UserService userService) {
         this.userService = userService;
-        this.oAuthLoginService = oAuthLoginService;
     }
 
     @PostMapping(Path.USER_SIGN_UP)
-    public ResponseEntity<OAuth2AccessToken> register(@RequestBody final User user) {
-        final User u = userService.findOrCreate(user);
-        return oAuthLoginService.login(u);
+    @ResponseStatus(HttpStatus.CREATED)
+    public User register(@RequestBody final User user) {
+        return userService.findOrCreate(user);
     }
 }
